9cac1c3d1f3c8b22e5f05db305bb94cdd070982d
D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>
2020-05-10 18:14:09 +0200

    Changed LoadFontEx helper (0-pointer-check removed)
    Signed-off-by: D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>

--- src/pbhelper/pbhelper_LoadFontEx.c
+++ src/pbhelper/pbhelper_LoadFontEx.c
@@ -1,5 +1,6 @@
 ﻿#include "raylib_pb_helper.h"
 
 void pbhelper_LoadFontEx(Font* result, const char *fileName, int fontSize, int *fontChars, int charsCount) {
-    if( result && fileName && fontChars ) *result = LoadFontEx(fileName, fontSize, fontChars, charsCount);
+    // fontChars can be 0, don't check!
+    if( result && fileName ) *result = LoadFontEx(fileName, fontSize, fontChars, charsCount);
 }
